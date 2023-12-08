8d94b6dcb6345f20e53020bce277474eebe16572
Tapio Jokinen <tapio.jokinen@hotmail.com>
2023-12-08 09:54:57 +0200

    Day 2

---
+++ 2023/Day2_part1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+// ReSharper disable ForeachCanBeConvertedToQueryUsingAnotherGetEnumerator
+// ReSharper disable LoopCanBeConvertedToQuery
+
+namespace Day2_part1
+{
+    internal static class Program
+    {
+        public static bool IsPossible(string[] results)
+        {
+            var colors = new Dictionary<string, int>
+            {
+                { "red", 12 },
+                { "green", 13 },
+                { "blue", 14 }
+            };
+
+            foreach (var keyValuePair in colors)
+            {
+                foreach (var result in results)
+                {
+                    var count = int.Parse(result.Split(' ')[0].Trim());
+                    if (result.Contains(keyValuePair.Key) && count > keyValuePair.Value)
+                        return false;
+                }
+            }
+            return true;
+        }
+        
+        public static void Main(string[] args)
+        {
+            var path = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\inputs\day2.txt"));
+            var lines = File.ReadLines(path);
+
+
+            var sumOfIds = 0;
+
+            foreach (var line in lines)
+            {
+                var game = line.Split(':');
+                var gameId = int.Parse(string.Join("", game[0].Where(c => int.TryParse(c.ToString(), out _))));
+
+                var results = game[1].Split(';');
+                var resultsString = string.Join(",", results);
+                var resultsCommaSplit = resultsString.Split(',');
+                var resultsSanitized = resultsCommaSplit.Select(s => s.Trim()).ToArray();
+
+                if (IsPossible(resultsSanitized))
+                    sumOfIds += gameId;
+            }
+            
+            Console.WriteLine(sumOfIds);
+        }
+    }
+}
---
+++ 2023/Day2_part2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day2_part2
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var path = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\inputs\day2.txt"));
+            var lines = File.ReadLines(path);
+
+            var sumOfMultiplies = 0;
+
+            foreach (var line in lines)
+            {
+                var game = line.Split(':');
+                var gameId = int.Parse(string.Join("", game[0].Where(c => int.TryParse(c.ToString(), out _))));
+
+                var results = game[1].Split(';');
+                var resultsString = string.Join(",", results);
+                var resultsCommaSplit = resultsString.Split(',');
+                var resultsSanitized = resultsCommaSplit.Select(s => s.Trim()).ToArray();
+
+                var minRed = 0;
+                var minGreen = 0;
+                var minBlue = 0;
+
+                foreach (var result in resultsSanitized)
+                {
+                    var count = int.Parse(result.Split(' ')[0].Trim());
+
+                    if (result.Contains("red") && minRed < count)
+                        minRed = count;
+                    
+                    if (result.Contains("green") && minGreen < count)
+                        minGreen = count;
+                    
+                    if (result.Contains("blue") && minBlue < count)
+                        minBlue = count;
+                }
+
+                sumOfMultiplies += minRed * minGreen * minBlue;
+            }
+            Console.WriteLine(sumOfMultiplies);
+        }
+    }
+}
